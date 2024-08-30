b943429809af63ecaee69413588b7e98de6b081b
Joe Roberts <joe@engiqueer.net>
2024-08-30 16:27:23 +0100

    Update VPC to include IPv4 CIDR

--- terraform/modules/ec2-instance/iam.tf
+++ terraform/modules/ec2-instance/iam.tf
@@ -3,7 +3,7 @@ resource "aws_iam_role" "this" {
     Version = "2012-10-17"
     Statement = [
       {
-        Effect    = "Allow"
+        Effect = "Allow"
         Principal = {
           Service = "ec2.amazonaws.com"
         }
--- terraform/modules/vpc/vpc.tf
+++ terraform/modules/vpc/vpc.tf
@@ -1,3 +1,4 @@
 resource "aws_vpc" "this" {
   assign_generated_ipv6_cidr_block = true
+  cidr_block                       = "10.0.0.0/16"
 }
