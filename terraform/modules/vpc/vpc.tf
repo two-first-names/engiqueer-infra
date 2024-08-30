811356070117eebec32a2ffc361641719c23f857
Joe Roberts <joe@engiqueer.net>
2024-08-30 16:43:30 +0100

    Specify CIDRs for subnets

--- terraform/modules/vpc/vpc.tf
+++ terraform/modules/vpc/vpc.tf
@@ -11,6 +11,9 @@ resource "aws_subnet" "subnet" {
   count  = length(data.aws_availability_zones.available.names)
   vpc_id = aws_vpc.vpc.id
 
+  cidr_block      = cidrsubnet(aws_vpc.vpc.cidr_block, 8, count.index)
+  ipv6_cidr_block = cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 8, count.index)
+
   availability_zone               = data.aws_availability_zones.available.names[count.index]
   assign_ipv6_address_on_creation = true
 }
