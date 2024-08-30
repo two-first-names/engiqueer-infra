77b21c738598184df0715def733be97aa9d6387e
Joe Roberts <joe@engiqueer.net>
2024-08-30 16:16:20 +0100

    Initial commit

---
+++ terraform/modules/ec2-instance/iam.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_role" "this" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_managed_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.this.name
+}
---
+++ terraform/modules/vpc/vpc.tf
@@ -0,0 +1,3 @@
+resource "aws_vpc" "this" {
+  assign_generated_ipv6_cidr_block = true
+}
