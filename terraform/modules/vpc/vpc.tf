e7bb3e32f63c7f9e67199d2097c62431ada5b6a0
Joe Roberts <joe@engiqueer.net>
2024-08-30 16:44:25 +0100

    Fix route table

--- terraform/modules/vpc/vpc.tf
+++ terraform/modules/vpc/vpc.tf
@@ -30,7 +30,7 @@ resource "aws_route_table" "route_table" {
   vpc_id = aws_vpc.vpc.id
 
   route {
-    cidr_block = "0.0.0.0"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 
