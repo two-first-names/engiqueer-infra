79ed054cd6af28238445eae4c2b19edb515e18d4
Joe Roberts <joe@engiqueer.net>
2024-08-30 17:15:08 +0100

    Associate public IP address with instance;

--- terraform/modules/ec2-instance/ec2.tf
+++ terraform/modules/ec2-instance/ec2.tf
@@ -15,9 +15,10 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "this" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_id
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  associate_public_ip_address = true
 
   user_data = templatefile("${path.module}/user-data.sh", {})
 
